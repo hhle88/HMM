a99c6deffe0a3658358d7b1b1f47d14d71e61235
Hoang-Hiep Le <31334260+hhle88@users.noreply.github.com>
2017-09-20 10:11:41 +0800

    Update EM_HMM_isolated_digit_main.m

--- EM_HMM_isolated_digit_main.m
+++ EM_HMM_isolated_digit_main.m
@@ -5,16 +5,16 @@ function EM_HMM_isolated_digit_main()
 % version 1 (2017-08-25)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-DIM = 39; % dimension of a feature vector
-num_of_model = 10; % number of models: digit '0', digit '1',... digit '9'
 main_dr_wav2mfcc_e_d_a;
 generate_selected_TI_isolated_digits_testing_list_mat;
 generate_selected_TI_isolated_digits_training_list_mat;
 % generate_trainingfile_list;
 % generate_testingfile_list;
-training_file_list = 'trainingfile_list.mat';
-testing_file_list = 'testingfile_list.mat';
+training_file_list_name = 'trainingfile_list.mat';
+testing_file_list_name = 'testingfile_list.mat';
 
+DIM = 39; % dimension of a feature vector
+num_of_model = 11; % number of models: digit '1',... digit '9', digit '0', digit 'zero'
 num_of_state_start = 12; 
 num_of_state_end = 15;
 
@@ -22,8 +22,8 @@ accuracy_rate = zeros(1,num_of_state_end);
 
 for num_of_state = num_of_state_start : num_of_state_end; 
     % notice: number of state does not including START and END node in HMM
-    HMM = EM_HMMtraining(training_file_list, DIM, num_of_model, num_of_state);  % training phase
-    accuracy_rate(num_of_state) = HMMtesting(HMM, testing_file_list);                       % testing phase
+    HMM = EM_HMMtraining(training_file_list_name, DIM, num_of_model, num_of_state);  % training phase
+    accuracy_rate(num_of_state) = HMMtesting(HMM, testing_file_list_name);                       % testing phase
     fprintf('num_of_state: %d, accuracy_rate: %f\n',num_of_state, accuracy_rate(num_of_state));
     save accuracy_rate;    
 end
